1adb23bd8a3fd82ffa50641b220b29cea55e51a5
MarioBroi <inoix16@gmail.com>
2024-08-31 15:12:20 +0200

    add form for the places

--- src/views/AppForm.vue
+++ src/views/AppForm.vue
@@ -9,20 +9,32 @@ export default {
       description: '',
       startDate: '',
       endDate: '',
+      places: [],
     };
   },
   methods: {
+    addPlace() {
+      this.places.push({
+        title: "",
+        description: "",
+        image: "",
+        notes: "",
+        rating: "",
+        is_complete: false
+      })
+    },
     async createTrip() {
       const newTrip = {
         title: this.title,
         description: this.description,
         start_date: this.startDate,
-        end_date: this.endDate
+        end_date: this.endDate,
+        places: this.places,
       };
       const response = await axios
         .post('http://127.0.0.1:8000/api/trips', newTrip);
       this.$router.push(`/trip`);
-    }
+    },
   }
 };
 </script>
@@ -42,8 +54,7 @@ export default {
 
       <div class="mb-5">
         <label for="description" class="form-label">Descrizione</label>
-        <textarea v-model="description" class="form-control" name="description" id="description" rows="3"
-          required></textarea>
+        <textarea v-model="description" class="form-control" name="description" id="description" rows="3"></textarea>
       </div>
 
       <div class="mb-5">
@@ -58,7 +69,44 @@ export default {
           required />
       </div>
 
-      <button type="submit" class="btn btn-primary" @click="createTrip()">
+      <div v-for="(place, index) in places" :key="index">
+        <h3>{{ index + 1 }} Tappa</h3>
+
+        <div class="mb-3">
+          <label for="place-name" class="form-label">Nome della tappa</label>
+          <input v-model="place.title" type="text" class="form-control" name="place-name" id="place-name"
+            aria-describedby="helpId" required />
+        </div>
+        <div class="mb-3">
+          <label for="place-description" class="form-label">Descrizione della tappa</label>
+          <textarea v-model="place.description" class="form-control" name="place-description" id="place-description"
+            rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="place-img" class="form-label">Aggiungi il link di una immagine</label>
+          <input v-model="place.image" type="text" class="form-control" name="place-img" id="place-img"
+            aria-describedby="place-img" />
+        </div>
+        <div class="mb-3">
+          <label for="placeNotes" class="form-label">Note</label>
+          <textarea v-model="place.notes" class="form-control" name="placeNotes" id="placeNotes" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="placeRating" class="form-label">Inserisci un voto da 1 a 5</label>
+          <input v-model="place.rating" type="number" class="form-control" name="placeRating" id="placeRating"
+            aria-describedby="helpId" min="0" max="5" />
+        </div>
+        <div class="form-check mb-3">
+          <label class="form-check-label" for="place-completed"> Completato </label>
+          <input v-model="place.is_complete" class="form-check-input" type="checkbox" value="" id="place-completed" />
+        </div>
+      </div>
+
+      <button @click.prevent="addPlace" class="btn btn-dark mx-2">
+        Aggiungi posto
+      </button>
+
+      <button type="submit" class="btn btn-primary">
         Crea viaggio
       </button>
     </form>
